531d7a2c1330e8dd8eed55b98ca7e164f87b65bd
Antonio D. Salcedo <antony9409@gmail.com>
2021-08-15 12:55:53 -0500

    no route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route } from 'react-router'
+// import { Route } from 'react-router'
 import { Layout } from './components/Layout'
 import Products from './components/Products'
 import Login from './components/Login'
@@ -7,18 +7,19 @@ import Submit from './components/Submit'
 import Category from './components/Category'
 import User from './components/User'
 import Permissions from './components/Permissions'
-//import './custom.css'
+import './custom.css'
 
 
 const App = () => {
   return (
     <div>
-      <Route exact path="/" component={Login} />
+      <Login />
+      {/* <Route exact path="/" component={Login} />
       <Route path="/Submit" component={Submit} />
       <Route path="/Categories" component={Category} />
       <Route path="/Products" component={Products} />
       <Route path="/User" component={User} />
-      <Route path="/Permissions" component={Permissions} />
+      <Route path="/Permissions" component={Permissions} /> */}
     </div>
   )
 }
