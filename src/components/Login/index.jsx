67e0524567f721fc220997323a06a1e603d855d7
Antonio D. Salcedo <antony9409@gmail.com>
2021-08-15 13:13:01 -0500

    no props

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,3 +1,9 @@
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./login.css";
+import "./Input/Input.css";
+import "./../../styles/react-confirm-alert.css";
+
 const Login = () => {
   return (
    <div>{"Hola"}</div>
