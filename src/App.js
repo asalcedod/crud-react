2c23df6e362c32a8e84cd5d49c6a9b984241ff29
Antonio D. Salcedo <antony9409@gmail.com>
2021-08-15 12:48:12 -0500

    old app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <Route exact path="/" component={Login} />
-      <Route path="/Submit" component={Submit} />
-      <Route path="/Categories" component={Category} />
-      <Route path="/Products" component={Products} />
-      <Route path="/User" component={User} />
-      <Route path="/Permissions" component={Permissions} />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
